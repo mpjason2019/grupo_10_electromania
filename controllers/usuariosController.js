22773b46af3100f698c54c863f5772ec55cf060f
Jeisson Medina <medinapjason@gmail.com>
2021-03-15 19:25:20 -0300

    adelanto de register

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -1,3 +1,10 @@
+const bcryptjs = require('bcryptjs');
+const {
+	validationResult
+} = require('express-validator');
+
+const User = require('../models/User');
+
 const usuariosController ={
 
     login:  (req, res) =>{
@@ -5,7 +12,42 @@ const usuariosController ={
     },
     register: (req, res) =>{
         res.render('register');
+    },
+
+    processRegister: (req, res) => {
+		const resultValidation = validationResult(req);
+
+		if (resultValidation.errors.length > 0) {
+			return res.render('register', {
+				errors: resultValidation.mapped(),
+				oldData: req.body,
+                
+			});
+		}
+
+		let userInDB = User.findByField('email', req.body.email);
+
+		if (userInDB) {
+			return res.render('register', {
+				errors: {
+					email: {
+						msg: 'Este email ya está registrado'
+					}
+				},
+				oldData: req.body,
+                
+			}); 
+		}
+
+		let userToCreate = {
+			...req.body,
+			password: bcryptjs.hashSync(req.body.password, 10),
+			avatar: req.file.filename
+		}
+
+		let userCreated = User.create(userToCreate);
+
+		return res.redirect('login');
     }
 }
-
 module.exports=usuariosController;
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,13 +1,22 @@
 const express=require('express');
 const router= express.Router();
+
+//Controller
 const usuariosController= require('../controllers/usuariosController.js')
+// Middlewares
+const uploadFile = require('../middlewares/multerMiddleware');
+const validations = require('../middlewares/validateRegisterMiddleware');
+const guestMiddleware = require('../middlewares/guestMiddleware');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 
 
-router.get('/login',usuariosController.login);
+
+router.get('/login',guestMiddleware, usuariosController.login);
 
 
 router.get('/register', usuariosController.register);
+router.post('/register', uploadFile.single('avatar'), validations, usuariosController.processRegister);
 
 
 
